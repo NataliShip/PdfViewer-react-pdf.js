38b0d12e5333f6b4d8928604d2869bc05e8c7dbe
Nataliya Shipelova <natulechka_m.25@mail.ru>
2020-01-27 17:23:35 +0300

    feat (PdfViewer) setup project

---
+++ src/components/PdfViewer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './pdfViewer.sass'
+
+function PdfViewer() {
+  return (
+    <div className="root">
+      test
+    </div>
+  );
+}
+
+export default PdfViewer
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PdfViewer from './components/PdfViewer'
+
+ReactDOM.render(<PdfViewer />, document.getElementById('root'))
+
