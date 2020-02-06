6e7799532813bc416529f8b315592b26033979e9
Nataliya Shipelova <natulechka_m.25@mail.ru>
2020-02-06 10:38:03 +0300

    Updates

---
+++ precache-manifest.0ba4eaebd5a520e9738925be322f88bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0e6e7f8a9a6274d70b8700f2933e82",
+    "url": "/PdfViewer-react-pdf.js/index.html"
+  },
+  {
+    "revision": "4a34946e19c313922b1a",
+    "url": "/PdfViewer-react-pdf.js/static/css/main.cbb01e8f.chunk.css"
+  },
+  {
+    "revision": "db63a0aeff77b0e472d1",
+    "url": "/PdfViewer-react-pdf.js/static/js/2.211992d0.chunk.js"
+  },
+  {
+    "revision": "4a34946e19c313922b1a",
+    "url": "/PdfViewer-react-pdf.js/static/js/main.9792e537.chunk.js"
+  },
+  {
+    "revision": "6d634d463c714d432d27",
+    "url": "/PdfViewer-react-pdf.js/static/js/runtime-main.a1c03914.js"
+  },
+  {
+    "revision": "fe64df96ffcfa306b0e074a2657f603d",
+    "url": "/PdfViewer-react-pdf.js/static/media/arrow.fe64df96.svg"
+  },
+  {
+    "revision": "30be3229671ea091f5a384aeed53a00b",
+    "url": "/PdfViewer-react-pdf.js/static/media/download.30be3229.svg"
+  },
+  {
+    "revision": "d39c133a32c23d842f09b0717e99c84a",
+    "url": "/PdfViewer-react-pdf.js/static/media/fullscreen.d39c133a.svg"
+  }
+]);
