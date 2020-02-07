0443f4b82244c700bbbcb4e64acb7ae81ac2e6d8
Nataliya Shipelova <natulechka_m.25@mail.ru>
2020-02-07 17:16:58 +0300

    eat (PdfViewer) add tests for App

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/* eslint-env jest */
+import { shallow, mount } from 'enzyme'
+import React from 'react'
+import '@babel/polyfill'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
+import PdfViewer from './PdfViewer/PdfViewer'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({ adapter: new Adapter() })
+
+let appComponent
+
+describe('App component', () => {
+  describe('Given the component has been rendered', () => {
+    beforeEach(() => {
+      appComponent = shallow(<App/>)
+    })
+
+    it('Then main tag should be displayed', () => {
+      expect(appComponent.find('main').length).toBe(1)
+    })
+
+
+    it('Then two PdfViewer should be displayed', () => {
+      expect(appComponent.find(PdfViewer).length).toBe(2)
+    })
+  })
+})
